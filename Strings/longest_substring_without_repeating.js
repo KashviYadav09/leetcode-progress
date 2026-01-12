d79e36640768f4a47e7f298a224f9caa4fcda31c
KashviYadav09 <kashviyadav220451@acropolis.in>
2026-01-12 22:19:23 +0530

    Day 2 : Add function for longest substring without repeating
    Implement function to find length of longest substring without repeating characters.

---
+++ Strings/longest_substring_without_repeating.js
@@ -0,0 +1,21 @@
+// LeetCode 3 - Longest Substring Without Repeating Characters
+// Time: O(N)
+// Space: O(N)
+
+var lengthOfLongestSubstring = function (s) {
+    let set = new Set();
+    let left = 0;
+    let maxLen = 0;
+
+    for (let right = 0; right < s.length; right++) {
+        while (set.has(s[right])) {
+            set.delete(s[left]);
+            left++;
+        }
+
+        set.add(s[right]);
+        maxLen = Math.max(maxLen, right - left + 1);
+    }
+
+    return maxLen;
+};
