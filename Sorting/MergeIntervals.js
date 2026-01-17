a3adbc5deb918cf186e0875382409e7926e0f94c
Kashvi Yadav <kashviyadav220451@acropolis.in>
2026-01-17 16:49:28 +0530

    Implement merge intervals function

---
+++ Sorting/MergeIntervals.js
@@ -0,0 +1,16 @@
+var merge = function(intervals) {
+    intervals.sort((a, b) => a[0] - b[0]);
+    let result = [];
+
+    for (let interval of intervals) {
+        if (!result.length || result[result.length - 1][1] < interval[0]) {
+            result.push(interval);
+        } else {
+            result[result.length - 1][1] = Math.max(
+                result[result.length - 1][1],
+                interval[1]
+            );
+        }
+    }
+    return result;
+};
