af526e2ebfa1db27f48f2a7a6a40b07acf92e0eb
Kashvi Yadav <kashviyadav220451@acropolis.in>
2026-01-19 16:19:42 +0530

    Implement MinHeap class with basic operations

---
+++ Heap/├── MinHeap.js
@@ -0,0 +1,54 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+
+    pop() {
+        if (this.size() === 1) return this.heap.pop();
+        const min = this.heap[0];
+        this.heap[0] = this.heap.pop();
+        this.bubbleDown();
+        return min;
+    }
+
+    bubbleUp() {
+        let idx = this.size() - 1;
+        while (idx > 0) {
+            let parent = Math.floor((idx - 1) / 2);
+            if (this.heap[parent] <= this.heap[idx]) break;
+            [this.heap[parent], this.heap[idx]] = [this.heap[idx], this.heap[parent]];
+            idx = parent;
+        }
+    }
+
+    bubbleDown() {
+        let idx = 0;
+        while (true) {
+            let left = 2 * idx + 1;
+            let right = 2 * idx + 2;
+            let smallest = idx;
+
+            if (left < this.size() && this.heap[left] < this.heap[smallest]) smallest = left;
+            if (right < this.size() && this.heap[right] < this.heap[smallest]) smallest = right;
+
+            if (smallest === idx) break;
+            [this.heap[idx], this.heap[smallest]] = [this.heap[smallest], this.heap[idx]];
+            idx = smallest;
+        }
+    }
+}
+
+module.exports = MinHeap;
